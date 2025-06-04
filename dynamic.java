fe89b09e7af2c8a5d9fa1f7cedd68cfb1d13fdd3
BharathiKalamalla <bharathikalamalla@gmail.com>
2025-06-04 14:27:24 +0530

    Add files via upload

---
+++ dynamic.java
@@ -0,0 +1,16 @@
+import java.util.*
+public class Main
+{
+	public static void main(String[] args) {
+		int [a=new int[5];
+		Scanner sc=new Scaner (System.in);
+	for(int i=0;i,a.length;i++)	
+	{
+	    a[i]=sc.nextInt();
+	}
+	 for( int i=0;i<a.length;i++)
+	 {
+	     System.out.println(a[i]);
+	 }
+	  }
+	}
