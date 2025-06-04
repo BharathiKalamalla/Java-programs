91e776e1a45e9759ce3fe2f8f6cb07473f918447
BharathiKalamalla <bharathikalamalla@gmail.com>
2025-06-04 14:21:15 +0530

    Add files via upload

---
+++ copyarray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	int []a=new int[5];
+	int []b=new int[a.length];
+	Scanner sc=new Scanner (System.in);
+	for(int i=0;i<a.length;i++)	
+	{
+	    a[i]=sc.nextInt();
+	}
+	for(int i=0;i<a.length;i++)
+	{
+	    b[i]=a[i];
+	}
+	for(int i=1;i<a.length;i++)
+	{
+	    System.out.println(b[i]);
+	}
+	   
+	    
+	}
+	
+}
